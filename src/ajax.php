095c5ea428d79b3300c8060147a267676667067c
Walter A Jablonowski <31986246+walter-a-jablonowski@users.noreply.github.com>
2025-03-06 16:43:43 +0100

    Removed unneeded var

--- src/ajax.php
+++ src/ajax.php
@@ -7,15 +7,13 @@ try {
   $input  = json_decode( file_get_contents('php://input'), true);
   $action = $_SERVER['REQUEST_METHOD'] === 'POST' ? $input['action'] : $_GET['action'];
   
-  $handlerFile = "ajax/{$action}.php";
-  
-  if( ! file_exists($handlerFile) ) {
+  if( ! file_exists("ajax/{$action}.php") ) {
     http_response_code(400);
     echo json_encode(['error' => 'Invalid action']);
     exit;
   }
   
-  require_once $handlerFile;
+  require_once "ajax/{$action}.php";
 }
 catch( Exception $e ) {
   http_response_code(500);
