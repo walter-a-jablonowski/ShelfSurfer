d9739cb557420d6cfcff6a194586f435a987b29a
Walter A Jablonowski <31986246+walter-a-jablonowski@users.noreply.github.com>
2025-03-07 14:07:30 +0100

    Moved files

--- src/print.php
+++ src/print.php
@@ -43,7 +43,7 @@ foreach( $currentList as $item )
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Shelf Surfer - Print View</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link href="print.css?v=<?= time() ?>" rel="stylesheet">
+  <link href="styles/print.css?v=<?= time() ?>" rel="stylesheet">
 </head>
 <body>
   <div class="container-fluid mt-3 mb-4 px-4">
--- src/view.php
+++ src/view.php
@@ -6,7 +6,7 @@
   <title>Shelf Surfer</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" rel="stylesheet">
-  <link href="style.css?v=<?= time() ?>" rel="stylesheet">
+  <link href="styles/app.css?v=<?= time() ?>" rel="stylesheet">
 </head>
 <body>
 
